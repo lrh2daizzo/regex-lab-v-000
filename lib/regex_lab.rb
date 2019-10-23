bab67ac4f31174e12a80d02e668c47fec7f528d3
Luis Herrera <herrera.lrh@gmail.com>
2019-10-23 18:23:05 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,18 @@
+
 def starts_with_a_vowel?(word)
   word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun\w*ing\b/)
+def words_starting_with_un_and_ending_with_ing(words_string)
+  words_string.scan(/\bun\w+ing/)
 end
 
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+def words_five_letters_long(words_string)
+  words_string.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+[\.!?]$/) ? true : false
+  text.match(/^[A-Z].+[.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
