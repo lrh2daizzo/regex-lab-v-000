59b9dcd4d7d7e45ee92b52b2b3d9afc759b5e0c9
Luis Herrera <herrera.lrh@gmail.com>
2019-10-23 18:03:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,3 +6,7 @@ end
 def words_starting_with_un_and_ending_with_ing(words_string)
   words_string.scan(/\bun\w+ing/)
 end
+
+def words_five_letters_long(words_string)
+  words_string.scan(/\b\w{5}\b/)
+end
