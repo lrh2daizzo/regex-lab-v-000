fdc4d8ed68774fdd554dea0056dcc30a5fde7f39
Luis Herrera <herrera.lrh@gmail.com>
2019-10-23 18:19:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,6 +11,6 @@ def words_five_letters_long(words_string)
   words_string.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation(text)
+def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].+[.!?]$/) ? true : false
 end
