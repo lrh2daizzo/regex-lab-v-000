59919eabfc76c58398934087bce75777ccca37cd
lrh2daizzo <herrera.lrh@gmail.com>
2020-09-10 15:20:00 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
-
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]/) ? true : false
+  word.match(/\b[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(words_string)
@@ -11,8 +10,8 @@ def words_five_letters_long(words_string)
   words_string.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+[.!?]$/) ? true : false
+def first_word_capitalized_and_ends_with_punctuation?(sentence)
+  sentence.match(/^[A-Z].+[.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
